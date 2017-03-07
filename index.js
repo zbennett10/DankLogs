830a13af57bdeff572596cc7e83bde8e9427873c
Zachary Bennett <zbennett10@hotmail.com>
2017-03-07 10:58:09 -0500

    'updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = function({dispatch}) {
     return next => action => {
+        const returnValue = next(action);
         console.log('%cAction:','color: blue; font-size: x-large', action);
         console.log('%cAction type:', 'color: blue; font-size: x-large', action.type);
         console.log('%cAction payload:', 'color: blue; font-size: x-large', action.payload);
@@ -7,16 +8,13 @@ module.exports = function({dispatch}) {
             for(let prop in action.payload) {
                 console.log('%cProperty: Value', 'color: red; font-size: medium', `${prop}: ${action.payload[prop]}`);
             }
-            next(action);
         }
 
         if(action.payload.hasOwnProperty('length') && typeof action.payload !== 'string') {
             action.payload.forEach(item => {
                 console.log(`%cItem at index ${action.payload.indexOf(item)}:`, item);
             });
-            next(action);
         }
-        next(action);
-
+        return returnValue;
     }
 }
