6a8ca46d0036daf2e4668470a75ca6262acc4892
Zachary Bennett <zbennett10@hotmail.com>
2017-03-07 07:46:30 -0500

    initial commit

---
+++ dank_logs.js
@@ -0,0 +1,19 @@
+export default function({dispatch}) {
+    return next => action => {
+        console.log('%cAction:','color: blue; font-size: x-large', action);
+        console.log('%cAction type:', 'color: blue; font-size: x-large', action.type);
+        console.log('%cAction payload:', 'color: blue; font-size: x-large', action.payload);
+        if(!action.payload.hasOwnProperty('length')) {
+            for(let prop in action.payload) {
+                console.log('%cProperty: Value', 'color: red; font-size: medium', `${prop}: ${action.payload[prop]}`);
+            }
+        }
+
+        if(action.payload.hasOwnProperty('length') && typeof action.payload !== 'string') {
+            action.payload.forEach(item => {
+                console.log(`%cItem at index ${action.payload.indexOf(item)}:`, item);
+            });
+        }
+
+    }
+}
